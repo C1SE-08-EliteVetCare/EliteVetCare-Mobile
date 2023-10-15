b62da33e54b98bc4d059bc7ea6cf739171460d10
DuyThanh211 <hoduythanh2112002@gmail.com>
2023-10-15 13:28:41 +0700

    aaaaa

---
+++ app/src/main/java/com/example/elitevetcare/ResetPassWord.java
@@ -0,0 +1,14 @@
+package com.example.elitevetcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass_word);
+    }
+}
