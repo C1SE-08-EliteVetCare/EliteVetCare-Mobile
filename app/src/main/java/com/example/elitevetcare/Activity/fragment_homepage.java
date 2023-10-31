a821383d25e5541a16ba9d959cb71b435290f52c
DuyThanh211 <147648892+DuyThanh211@users.noreply.github.com>
2023-10-31 14:10:44 +0700

    cccomttaa

--- app/src/main/java/com/example/elitevetcare/Activity/fragment_homepage.java
+++ app/src/main/java/com/example/elitevetcare/Activity/fragment_homepage.java
@@ -12,10 +12,10 @@ import com.example.elitevetcare.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_schedule#newInstance} factory method to
+ * Use the {@link fragment_homepage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_schedule extends Fragment {
+public class fragment_homepage extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class fragment_schedule extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_schedule() {
+    public fragment_homepage() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class fragment_schedule extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_schedule.
+     * @return A new instance of fragment fragment_homepage.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_schedule newInstance(String param1, String param2) {
-        fragment_schedule fragment = new fragment_schedule();
+    public static fragment_homepage newInstance(String param1, String param2) {
+        fragment_homepage fragment = new fragment_homepage();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class fragment_schedule extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_homepage, container, false);
     }
 }
--- app/src/main/java/com/example/elitevetcare/Appointment/fragment_schedule.java
+++ app/src/main/java/com/example/elitevetcare/Appointment/fragment_schedule.java
@@ -1,4 +1,4 @@
-package com.example.elitevetcare.Activity;
+package com.example.elitevetcare.Appointment;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.elitevetcare.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_chatbot_list#newInstance} factory method to
+ * Use the {@link fragment_schedule#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_chatbot_list extends Fragment {
+public class fragment_schedule extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class fragment_chatbot_list extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_chatbot_list() {
+    public fragment_schedule() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class fragment_chatbot_list extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_chatbot_list.
+     * @return A new instance of fragment fragment_schedule.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_chatbot_list newInstance(String param1, String param2) {
-        fragment_chatbot_list fragment = new fragment_chatbot_list();
+    public static fragment_schedule newInstance(String param1, String param2) {
+        fragment_schedule fragment = new fragment_schedule();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class fragment_chatbot_list extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatbot_list, container, false);
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
     }
 }
